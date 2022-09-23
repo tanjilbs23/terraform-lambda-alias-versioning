51d2b7f595aaff10b71d5e669ed094264b20b783
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-23 19:06:11 +0600

    alias & versioning test-5

--- lambda/test_alias_versioning/main.tf
+++ lambda/test_alias_versioning/main.tf
@@ -3,10 +3,10 @@ module "lambda_function" {
 
   function_name = "test_alias_versioning"
   description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
+  handler       = "lambda_handler.lambda_handler"
   runtime       = "python3.9"
 
-  source_path = "lambda/src/test_alias_versioning"
+  source_path = "src/test_alias_versioning"
 
   tags = {
     Name = "test_alias_versioning_test"
