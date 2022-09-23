28964d69e2b4c9f812d6a01f41f944b00b6fb1c4
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-23 19:15:39 +0600

    alias & versioning test-8

--- lambda/test_alias_versioning/main.tf
+++ lambda/test_alias_versioning/main.tf
@@ -5,10 +5,29 @@ module "lambda_function" {
   description   = "My awesome lambda function"
   handler       = "lambda_handler.lambda_handler"
   runtime       = "python3.9"
+  publish = true
 
   source_path = "lambda/test_alias_versioning/src"
 
   tags = {
     Name = "test_alias_versioning_test"
   }
+}
+
+module "alias_no_refresh" {
+  source = "terraform-aws-modules/lambda/aws//modules/alias"
+
+  refresh_alias = false
+
+  name = "current-no-refresh"
+
+  function_name    = module.lambda_function.lambda_function_name
+  function_version = module.lambda_function.lambda_function_version
+
+  # allowed_triggers = {
+  #   AnotherAPIGatewayAny = {
+  #     service    = "apigateway"
+  #     source_arn = "arn:aws:execute-api:eu-west-1:135367859851:abcdedfgse/*/*/*"
+  #   }
+  # }
 }
